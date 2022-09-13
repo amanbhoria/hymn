fff3da84bc66ff3151d563704ac4433baec6a3aa
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2022-09-13 18:29:40 +0530

    updates

--- project practice/pp.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const { COPYFILE_EXCL } = fs.constants;
-
-// destination.txt will be created or overwritten by default.
-fs.copyFileSync('file1.txt', 'file2.txt');
-console.log('file1.txt was copied to file2.txt');
-
-// By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
-// fs.copyFileSync('source.txt', 'destination.txt', COPYFILE_EXCL);
